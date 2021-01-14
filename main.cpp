02efa177783810a9b67065c97b120b6693451be8
himal77 <purihimal9@gmail.com>
2021-01-14 18:38:10 +0100

    serial version with smooth iteration

--- main.cpp
+++ main.cpp
@@ -1,62 +1,89 @@
 #include <iostream>
-#include <stdlib.h>
+#include <iomanip>
+#include <cstdio>
 
 size_t SIZE = 4;
 size_t n = SIZE;
-size_t noOfIteration = 1;
+size_t noOfIteration = 2;
+size_t precision = 3;
 
 
 int main() {
+    // Declaring two matrix of same size
     double **A = new double *[SIZE];
+    double **B = new double *[SIZE];
+
 
     for (int i = 0; i < SIZE; i++) {
         A[i] = new double[SIZE];
+        B[i] = new double[SIZE];
     }
+
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            A[i][j] = 0;//rand() % 100;
+            if (i == 1 && j == 2) {
+                A[i][j] = 10.0;
+                B[i][j] = A[i][j];
+                continue;
+            }
+            A[i][j] = 0.0; //rand() % 100;
+            B[i][j] = A[i][j];
         }
     }
 
-    A[1][2] = 10;
 
-    double **B = A;
     // inserting into matrix
+
     // serial version
     for (int iteration = 0; iteration < noOfIteration; iteration++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
 
-                int second = 0;
-                if (i - 1 >= 0) second = B[i - 1][j];
-                int third = 0;
-                if (i + 1 < n) third = B[i + 1][j];
-                int fourth = 0;
-                if (j - 1 >= 0) fourth = B[i][j - 1];
-                int fifth = 0;
-                if (j + 1 < n) fifth = B[i][j + 1];
-
-                A[i][j] = 0.2 * (
+                // Adding new value to new B
+                B[i][j] = 0.2 * (
                         A[i][j] +
-                        second +
-                        third +
-                        fourth +
-                        fifth
-
+                        (i - 1 > -1 ? A[i - 1][j] : 0) + // if out of bound on left
+                        (i + 1 < n ? A[i + 1][j] : 0) + // if out of bound on right
+                        (j - 1 > -1 ? A[i][j - 1] : 0) + // if out of bound on top
+                        (j + 1 < n ? A[i][j + 1] : 0) // if out of bound on bottom
                 );
             }
         }
-        B = A;
-    }
-    // parallel version
 
-    // displaying matrix
+
+        // Assigning all value of iterated B to original A, for next iteration
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                A[i][j] = B[i][j];
+            }
+        }
+    }
+
+
+    // displaying matrix in serial version
+    std::cout << std::setprecision(precision) << std::fixed;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            std::cout << A[i][j] << " ";
+            std::cout << A[i][j] << "  ";
         }
         std::cout << "\n";
     }
+
+
+    // parallel version
+
+
+
+    // displaying matrix in parallel version
+//    std::cout << std::setprecision(precision) << std::fixed;
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n; j++) {
+//            std::cout << A[i][j] << "  ";
+//        }
+//        std::cout << "\n";
+//    }
+
+
     return 0;
 }
