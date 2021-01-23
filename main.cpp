444bf246c19cb3ede5a6f1ac630775fe6f4deebe
himal77 <purihimal9@gmail.com>
2021-01-23 17:39:57 +0100

    code clean up

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdio>
+#include <chrono>
 
 size_t SIZE = 4;
 size_t n = SIZE;
-size_t noOfIteration = 2;
+size_t noOfIteration = 3;
 size_t precision = 3;
 
 
@@ -32,37 +33,52 @@ int main() {
         }
     }
 
+    /////////////////////////////////////////////////
+    ///////////////Serial Version////////////////////
+    /////////////////////////////////////////////////
 
-    // inserting into matrix
-
-    // serial version
+    /**
+     *  inserting into matrix for serial version
+     *  @iteration: no of times the matrix will be iterated
+     *
+     *  @A: matrix which will store the data
+     *  @B: helper matrix for smoothing out the iteration
+     */
+    auto start = std::chrono::high_resolution_clock::now();
     for (int iteration = 0; iteration < noOfIteration; iteration++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
 
-                // Adding new value to new B
-                B[i][j] = 0.2 * (
-                        A[i][j] +
-                        (i - 1 > -1 ? A[i - 1][j] : 0) + // if out of bound on left
-                        (i + 1 < n ? A[i + 1][j] : 0) + // if out of bound on right
-                        (j - 1 > -1 ? A[i][j - 1] : 0) + // if out of bound on top
-                        (j + 1 < n ? A[i][j + 1] : 0) // if out of bound on bottom
+                // Adding new value to new A using B
+                A[i][j] = 0.2 * (
+                        B[i][j] +
+                        (i - 1 > -1 ? B[i - 1][j] : 0) +   // if out of bound on left
+                        (i + 1 < n ? B[i + 1][j] : 0) +   // if out of bound on right
+                        (j - 1 > -1 ? B[i][j - 1] : 0) +   // if out of bound on top
+                        (j + 1 < n ? B[i][j + 1] : 0)     // if out of bound on bottom
                 );
             }
         }
 
-
-        // Assigning all value of iterated B to original A, for next iteration
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                A[i][j] = B[i][j];
+        /**
+         * Assigning value of A to B for next iteration
+         * if its last iteration, no need to assign
+         */
+        if (iteration != noOfIteration - 1) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    B[i][j] = A[i][j];
+                }
             }
         }
     }
+    auto end = std::chrono::high_resolution_clock::now();
 
-
-    // displaying matrix in serial version
-    std::cout<<"Serial version: \n";
+    /**
+     * displaying matrix in serial version
+     * As well as the time it takes to compute
+     */
+    std::cout << "Serial version: \n";
     std::cout << std::setprecision(precision) << std::fixed;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -70,13 +86,26 @@ int main() {
         }
         std::cout << "\n";
     }
+    std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+              << std::endl;
 
 
-    // parallel version
+    /////////////////////////////////////////////////
+    ///////////////Parallel Version//////////////////
+    /////////////////////////////////////////////////
+    start = std::chrono::high_resolution_clock::now();
+    /**
+     * Parallel version using OpenCL
+     *
+     * TODO write the code for OpenCL
+     */
+    end = std::chrono::high_resolution_clock::now();
 
-
-
-    // displaying matrix in parallel version
+    /**
+     * Displaying matrix in parallel version.
+     * The time it takes to compute.
+     */
+/**
     std::cout<<"Parallel version: \n";
     std::cout << std::setprecision(precision) << std::fixed;
     for (int i = 0; i < n; i++) {
@@ -85,7 +114,8 @@ int main() {
         }
         std::cout << "\n";
     }
-
+    std::cout<<"Time taken: "<<std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()<<std::endl;
+*/
 
     return 0;
 }
